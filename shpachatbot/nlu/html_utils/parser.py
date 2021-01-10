f8a56138ccffc64281a445d476accacdfe8282e4
ali <akyegane@gmail.com>
2021-01-10 11:56:28 +0330

    NewFeature: support for multi value and read from specified attribute

--- shpachatbot/nlu/html_utils/parser.py
+++ shpachatbot/nlu/html_utils/parser.py
@@ -55,6 +55,16 @@ class HtmlField:
     def selector(self):
         return self._selector
 
+    def __contains__(self, attr_key):
+        if attr_key in self._attrs:
+            return True
+        return False
+
+    def __getitem__(self, attr_key):
+        if attr_key in self:
+            return self._attrs[attr_key]
+        return None
+
 
 class HtmlParser:
     def __init__(self, html_path: str):
@@ -72,8 +82,11 @@ class HtmlParser:
         for field_name, field in self._fields.items():
             yield field
 
-    def add_field(self, *, field_name: str, css_selector: str, is_multi: bool = False):
-        self._fields[field_name] = HtmlField(name=field_name, selector=css_selector, is_multi=is_multi)
+    def add_field(self, *, field_name: str, css_selector: str, is_multi: bool = False, value_from=None):
+        self._fields[field_name] = HtmlField(name=field_name,
+                                             selector=css_selector,
+                                             is_multi=is_multi,
+                                             value_from=value_from)
 
     def parse(self):
         result = {}
@@ -82,8 +95,22 @@ class HtmlParser:
         return result
 
     def _parse_field(self, field_name):
-        text = self._soup.select_one(self[field_name].selector).text
-        return text
+        field = self[field_name]
+        selected = []
+        if 'is_multi' in field and field['is_multi']:
+            selected = [e for e in self._soup.select(field.selector)]
+        else:
+            selected.append(self._soup.select_one(self[field_name].selector))
+
+        if 'value_from' in field and field['value_from']:
+            selected = [s[field['value_from']] for s in selected if s.has_attr(field['value_from'])]
+        else:
+            selected = [s.text for s in selected]
+
+        if len(selected) == 1:
+            return selected[0]
+        else:
+            return selected
 
 
 class HmashahriParser(HtmlParser):
@@ -94,9 +121,26 @@ class HmashahriParser(HtmlParser):
         super().add_field(field_name='post_content', css_selector='div.item-text')
 
 
+class Way2PayParser(HtmlParser):
+    def __init__(self, html_path: str):
+        super(Way2PayParser, self).__init__(html_path)
+        super().add_field(field_name='post_badges', css_selector='div.post-header-title span.term-badge', is_multi=True)
+        super().add_field(field_name='post_title', css_selector='h1.single-post-title')
+        super().add_field(field_name='post_tags', css_selector='.post-tags a', is_multi=True)
+        # super().add_field(field_name='post_content', css_selector='.entry-content')
+        super().add_field(field_name='post_date', css_selector='time.post-published', value_from='datetime')
+
+
 if __name__ == "__main__":
-    html_path_base = 'D:\\_temp\\crawler\\hamshahri'
+    # html_path_base = 'D:\\_temp\\crawler\\hamshahri'
+    # for file in os.listdir(html_path_base):
+    #     hamshahri_html = HmashahriParser(os.path.join(html_path_base, file))
+    #     fields = hamshahri_html.parse()
+    #     print(fields)
+
+    html_path_base = 'D:\\_temp\\crawler\\way2pay\\posts'
     for file in os.listdir(html_path_base):
-        hamshahri_html = HmashahriParser(os.path.join(html_path_base, file))
-        fields = hamshahri_html.parse()
+        html_parser = Way2PayParser(os.path.join(html_path_base, file))
+        fields = html_parser.parse()
+        fields['id'] = file
         print(fields)
